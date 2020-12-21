51872326b12c42ed5a88f4435d8b714049bddece
House <House@DESKTOP-RBMRQN3>
2020-12-21 20:47:24 +0530

    Name and age

--- Student/src/main/java/com/lti/StudentDetails.java
+++ Student/src/main/java/com/lti/StudentDetails.java
@@ -4,9 +4,13 @@ public class StudentDetails {
       
 	public static String getName() {
 		return "sathya";
-	}
+			}
+	public static String getAge() {
+		return "22";
+			}
 	public static void main(String[] args) {
 	 System.out.println("Name "+getName());
+	 System.out.println("Age "+getAge());
 
 	}
 
