f592c49bec8df2d8099b3fcc47c0655a2192d2ad
House <House@DESKTOP-RBMRQN3>
2020-12-21 20:29:47 +0530

    Student-Maven

---
+++ Student/src/main/java/com/lti/StudentDetails.java
@@ -0,0 +1,13 @@
+package com.lti;
+
+public class StudentDetails {
+      
+	public static String getName() {
+		return "sathya";
+	}
+	public static void main(String[] args) {
+	 System.out.println("Name "+getName());
+
+	}
+
+}
